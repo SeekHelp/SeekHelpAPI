b0213b04cb432ae876d125860c390649740906db
Frederik Egegaard Hjorth <feh@netcompany.com>
2020-03-26 10:25:51 +0100

    2 model klasser implemteret

---
+++ Models/HelperModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SeekHelpServer.Models
+{
+    public class HelperModel
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string[] Education { get; set; }
+        public string[] Experience { get; set; }
+        public string Availability { get; set; }
+        public string Ranking { get; set; }
+    }
+}
---
+++ Models/SeekerModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SeekHelpServer.Models
+{
+    public class SeekerModel
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
